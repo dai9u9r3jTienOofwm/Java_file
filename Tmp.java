ebda2ae95b06de8cf8e3b6ad7629b40faf071d29
dai9u9r3jTienOofwm <ti1306en6002@gmail.com>
2025-02-27 07:32:55 +0700

    Create Tmp.java
    test

---
+++ Tmp.java
@@ -0,0 +1,13 @@
+public class Tmp {
+    // Type your main code here
+
+
+    public static String printHello() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+
+    }
+} 
